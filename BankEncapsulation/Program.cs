8a387c3f6b3708d32c3fac93257bc127712c0b84
Brett Lazarine <jblazarine@gmail.com>
2023-02-11 13:20:19 -0600

    add console interactivity

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -5,8 +5,19 @@
         static void Main(string[] args)
         {
             var account = new BankAccount();
-            account.Deposit(1);
-            Console.WriteLine(account.GetBalance());
+            Console.Write("How much would you like to deposit: ");
+            var response = Console.ReadLine();
+            double depositAmount;
+            bool isNumber;
+            isNumber = double.TryParse(response, out depositAmount);
+            while (isNumber == false)
+            {
+                Console.WriteLine("Please enter a valid amount: ");
+                response = Console.ReadLine();
+                isNumber = double.TryParse(response, out depositAmount);
+            }
+            account.Deposit(depositAmount);
+            Console.WriteLine($"Your new balance is ${account.GetBalance()}");
         }
     }
 }
